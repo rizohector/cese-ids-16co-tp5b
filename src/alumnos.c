67ca04580cac358fd74a0a37d012cf8332b4fad9
Hector RIZO <rizohector@gmail.com>
2021-12-11 04:19:14 -0300

    Se agregan los datos del alumno RIZO, Hector

--- src/alumnos.c
+++ src/alumnos.c
@@ -31,8 +31,15 @@ static const struct alumno_s ESTEBAN_VOLENTINI = {
     .documento = "23.517.968",
 };
 
+static const struct alumno_s HECTOR_RIZO = {
+    .apellidos = "RIZO",
+    .nombres = "Hector Ramon",
+    .documento = "25.522.523",
+};
+
 const alumno_t ALUMNOS[] = {
     &ESTEBAN_VOLENTINI,
+    &HECTOR_RIZO,
 };
 
 const int CANTIDAD_ALUMNOS = (sizeof(ALUMNOS) / sizeof(alumno_t));
